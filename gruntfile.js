b8ffbedc4d5791f657953734c111d62e77268b1c
Florian Loch <Florian.Loch@gmail.com>
2014-05-12 23:08:01 +0200

    Add build script for building minified version. Remove not needed files

---
+++ gruntfile.js
@@ -0,0 +1,77 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+        mangle: true
+      },
+      popup: {
+        dest: 'dist/md4c.popup.min.js',
+        src: [
+          'bower_components/jquery/dist/jquery.min.js', 
+          'bower_components/angular/angular.min.js',
+          "bower_components/angular-animate/angular-animate.min.js",
+          "js/popup.js"
+        ]
+      },
+      background: {
+        dest: 'dist/md4c.bg.min.js',
+        src: [
+          "js/background.js",
+          "js/mediathekCore.js",
+          "js/mediathekHandler/*"
+        ]
+      }
+    },
+    copy: {
+      bootstrap: {
+        src: "bower_components/bootstrap/dist/css/bootstrap.min.css", 
+        dest: 'dist/bs.min.css',
+      },
+      images: {
+        src: "images/*",
+        dest: "dist/"
+      },
+      manifest: {
+        src: 'manifest.json',
+        dest: 'dist/manifest.json',
+        options: {
+          process: function (content) {
+            var d = JSON.parse(content);
+            d.background.scripts = ['md4c.bg.min.js'];
+            return JSON.stringify(d);
+          }
+        }
+      },
+      popup: {
+        src: "popup.html",
+        dest: "dist/popup.html",
+        options: {
+          process: function (content) {
+            return content.replace(/<!-- START OF INCLUDES -->[\s\S]*<!-- END OF INCLUDES -->/, "<script type=\"text/javascript\" src=\"md4c.popup.min.js\"></script><link rel=\"stylesheet\" type=\"text/css\" href=\"bs.min.css\">");
+          }
+        }
+      }      
+    },
+    removeDir: {
+      dist: {
+        src: "dist"
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // Default task(s).
+  grunt.registerMultiTask("removeDir", "Removes a directory", function () {
+    var rimraf = require('rimraf');
+    rimraf.sync(this.data.src);
+  });
+
+  grunt.registerTask('build', ['removeDir', 'uglify', 'copy']);
+};  
--- js/popup.js
+++ js/popup.js
@@ -66,27 +66,3 @@ angular.module("md4c").animation(".plainfade", function () {
 		}
 	};
 });
-
-// $(function init () {
-// 	console.info("Test");
-// 	var cntnr = $("#container");
-
-// 	console.info(cntnr);
-
-// 	videoUrls = bg.mC.getVideoFileURLs(function (videoUrls_ar) {
-// 		for (var i = 0; i < videoUrls_ar.length; i++) {
-// 			var btn = $("<button>" + videoUrls_ar[i].desc + "</button>").click((function (i) {
-// 				return function () {
-// 					console.log(videoUrls_ar[i]);
-// 					bg.mC.downloadVideo(videoUrls_ar[i]);
-// 				}
-// 			})(i));
-// 			cntnr.append(btn);
-// 			console.log(btn);
-// 			console.info("appended button");
-// 		}
-
-// 		console.log("Callback called");
-// 	});
-// });
-
